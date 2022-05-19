85e30f402ba190211af9dfa16301280b3b884aab
LinathiMrulekana <102727009+LinathiMrulekana@users.noreply.github.com>
2022-05-19 09:41:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayNum = { 2, 4, 6, 9 };
+            bool oddNumber = false;
+            foreach (int num in arrayNum)
+            {
+                if (num %2 == 0)
+                {
+                    oddNumber = true;
+                    break;
+                }
+            }
+            if (oddNumber)
+            {
+                Console.WriteLine("There is an odd number in the array");
+            } else
+            {
+                Console.WriteLine("There is no odd number in the array.");
+            }
+        }
+            
+      
+        
+    }
+}
